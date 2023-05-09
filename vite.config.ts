477ae976e7257385a9d053be96e48c6bb82e84ad
zch1999 <1901394767@qq.com>
2023-05-09 12:32:27 +0800

    feat: 修改vite config

--- vite.config.ts
+++ vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
         open: true,
         proxy: {
             '/api/': {
-                target: 'http://localhost:3000/',
+                target: 'http://localhost:5173/',
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, '')
             }
