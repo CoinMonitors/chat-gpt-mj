cd69e98b017363c4184d0f4e7ecf3ea51f9523a8
zch1999 <1901394767@qq.com>
2023-05-14 19:27:25 +0800

    feat: 创作模块

--- src/views/start-creation/createForm.vue
+++ src/views/start-creation/createForm.vue
@@ -2,18 +2,38 @@
  * @Author: zch1999 1901394767@qq.com
  * @Date: 2023-05-04 17:52:03
  * @LastEditors: zch1999 1901394767@qq.com
- * @LastEditTime: 2023-05-04 17:53:09
+ * @LastEditTime: 2023-05-14 19:26:45
  * @FilePath: /chat-gpt-mj/src/views/start-creation/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
     <div class="start-creation">
-        <el-form :model="resParams" v-if="modelParam.length > 0">
+        <el-form :model="baseData" class="base">
+            <el-form-item class="base-item" prop="prompt">
+                <el-input type="textarea" class="base-input" v-model="baseData.prompt"></el-input>
+            </el-form-item>
+            <el-form-item class="base-item" prop="imagePrompt">
+                <!-- <el-input type="textarea" class="base-input" v-model="baseData.imagePrompt"></el-input> -->
+                <el-upload
+                    class="avatar-uploader"
+                    action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+                    :show-file-list="false"
+                    :on-success="handleAvatarSuccess"
+                    :before-upload="beforeAvatarUpload"
+                >
+                    <img v-if="baseData.imagePrompt" :src="baseData.imagePrompt" class="avatar" />
+                    <el-icon v-else class="avatar-uploader-icon">+</el-icon>
+                </el-upload>
+            </el-form-item>
+            <el-form-item class="base-item" prop="imageUrlPrompt">
+                <el-input type="textarea" class="base-input" v-model="baseData.imageUrlPrompt"></el-input>
+            </el-form-item>
+        </el-form>
+        <el-form :model="resParams" v-if="modelParam.length > 0" class="params" :label-position="'top'">
             <el-form-item
                 v-for="(item, index) in modelParam"
                 :key="index"
                 :label="item.nameZh"
-                :label-position="'top'"
                 class="create-item"
                 :prop="resParams[item.id]"
             >
@@ -45,6 +65,9 @@ const props = defineProps({
 const resParams = ref<{
     [key: string]: any;
 }>({});
+const baseData = ref<{
+    [key: string]: any;
+}>({});
 const modelParam = ref([]);
 const getData = async () => {
     const res = await nsCommonLoginPost.request();
@@ -53,7 +76,7 @@ const getData = async () => {
         console.log(res.data, creation, modelParams); //jing-log
         modelParam.value = modelParams;
         modelParams.forEach((item: { [key: string]: any }) => {
-            resParams.value[item.id] = '2';
+            resParams.value[item.id] = item.defaultValue;
         });
     }
 };
@@ -63,23 +86,75 @@ onMounted(() => {
 });
 
 const submitForm = async () => {
-    const res = await nsCommonLoginPost.creteDrawPost({
-        modelId: 'openjourney',
-        modelParams: resParams.value
-    });
-    console.log(resParams.value, res, 'res');
-    props.changeTaskId(res.data.taskId);
-    // const imgRes = await nsCommonLoginPost.getDrawResult(res.data.taskId);
-    // console.log(imgRes, 'img');
+    console.log(resParams.value, baseData.value, 'res');
+    // const res = await nsCommonLoginPost.creteDrawPost({
+    //     modelId: 'openjourney',
+    //     modelParams: resParams.value
+    // });
+    // props.changeTaskId(res.data.taskId);
 };
 </script>
 
 <style lang="less" scoped>
 .start-creation {
-    width: 60%;
+    width: 100%;
     min-width: 600px;
+    display: flex;
+    align-items: center;
 }
 .create-item {
     width: 100%;
+    margin-bottom: 10px;
+}
+.base {
+    width: 70%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    // flex-wrap: wrap;
+    .base-item {
+        width: 30%;
+        .base-input {
+            height: 250px;
+            :deep(.el-textarea__inner) {
+                height: 100%;
+                resize: none;
+                color: #ffffff;
+                background-color: #5a5a56;
+                box-shadow: 0 0 0 0px var(--el-input-border-color, var(--el-border-color)) inset;
+            }
+        }
+        .avatar-uploader {
+            width: 100%;
+            :deep(.el-upload) {
+                width: 100%;
+                border-radius: 6px;
+                cursor: pointer;
+                position: relative;
+                overflow: hidden;
+                transition: var(--el-transition-duration-fast);
+            }
+        }
+    }
+}
+.params {
+    width: 30%;
+    margin-left: 50px;
+    :deep(.el-form-item__label) {
+        color: #ffffff;
+    }
+}
+
+.avatar-uploader .el-upload:hover {
+    border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 100%;
+    height: 250px;
+    text-align: center;
+    background-color: #5a5a56;
 }
 </style>
--- src/views/start-creation/index.vue
+++ src/views/start-creation/index.vue
@@ -2,7 +2,7 @@
  * @Author: zch1999 1901394767@qq.com
  * @Date: 2023-05-04 17:52:03
  * @LastEditors: zch1999 1901394767@qq.com
- * @LastEditTime: 2023-05-04 17:53:09
+ * @LastEditTime: 2023-05-14 19:24:51
  * @FilePath: /chat-gpt-mj/src/views/start-creation/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -26,7 +26,9 @@ const changeTaskId = (value: string) => {
 <style scoped lang="less">
 .create {
     display: flex;
-    margin: 40px;
+    height: 100%;
+    padding: 40px;
+    background-color: #363634;
     .left {
         width: 60%;
     }
