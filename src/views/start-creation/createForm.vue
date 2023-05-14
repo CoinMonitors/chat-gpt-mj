ddab038a77fdf79ba9c762083c724e798857ad79
zch1999 <1901394767@qq.com>
2023-05-14 19:46:42 +0800

    feat: 创作模块

--- src/views/start-creation/createForm.vue
+++ src/views/start-creation/createForm.vue
@@ -2,7 +2,7 @@
  * @Author: zch1999 1901394767@qq.com
  * @Date: 2023-05-04 17:52:03
  * @LastEditors: zch1999 1901394767@qq.com
- * @LastEditTime: 2023-05-14 19:26:45
+ * @LastEditTime: 2023-05-14 19:44:51
  * @FilePath: /chat-gpt-mj/src/views/start-creation/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -86,12 +86,12 @@ onMounted(() => {
 });
 
 const submitForm = async () => {
+    const res = await nsCommonLoginPost.creteDrawPost({
+        modelId: 'openjourney',
+        modelParams: { ...resParams.value, ...baseData.value }
+    });
+    props.changeTaskId(res.data.taskId);
     console.log(resParams.value, baseData.value, 'res');
-    // const res = await nsCommonLoginPost.creteDrawPost({
-    //     modelId: 'openjourney',
-    //     modelParams: resParams.value
-    // });
-    // props.changeTaskId(res.data.taskId);
 };
 </script>
 
